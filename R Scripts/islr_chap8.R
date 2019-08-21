0016b008639b58461306eccd6b69410839667ab6
jkam32 <jkam32@siswa.um.edu.my>
2019-08-21 18:40:23 +0800

    chap 8 lab : boosting

--- R Scripts/islr_chap8.R
+++ R Scripts/islr_chap8.R
@@ -1,6 +1,9 @@
 # require R version 3.6.0 and above
 library(tree)
 library(ISLR)
+library(MASS)
+library(randomForest)
+library(gbm)
 
 # Can't reproduce the example in ISLR Chap 8 tree part;
 # Pruned tree has a lower accuracy than original tree
@@ -54,7 +57,7 @@ table(y_test, new_pred)
 # (89 + 62) / 200 
 
 #### Regression Tree ####
-library(MASS)
+# library(MASS)
 set.seed(1)
 train <- sample(1:nrow(Boston), nrow(Boston) / 2)
 tree_boston <- tree(medv ~ ., data = Boston, subset = train)
@@ -80,3 +83,41 @@ boston_test <- Boston$medv[-train]
 plot(yhat, boston_test)
 abline(0, 1)
 sqrt(mean((yhat - boston_test) ** 2))
+
+#### Random Forest ####
+# first try bagging (i.e. m = p, use all predictors for each tree)
+bag_boston <- randomForest(medv ~ ., data = Boston, subset = train, mtry = 13,
+                           importance = TRUE)
+bag_boston
+yhat_bag <- predict(bag_boston, newdata = Boston[-train, ])
+sqrt(mean((boston_test - yhat_bag) ** 2))
+
+# random forest
+
+# The function importance(randomForest) outputs two things:
+# (1) the mean decrease in accuracy on oob samples if the variable is excluded 
+#     from the model (so the larger the %, the more important the variable)
+# (2) the total decrease in node impurity that results from splits over that variable,
+#     averaged over all trees
+# Just use varImpPlot(randomForest) for a shortcut in the results
+rf_boston <- randomForest(medv ~ ., data = Boston, subset = train, mtry = 6,
+                          importance = TRUE)
+yhat_rf <- predict(rf_boston, newdata = Boston[-train, ])
+sqrt(mean((boston_test - yhat_rf) ** 2))
+importance(rf_boston) 
+varImpPlot(rf_boston)
+
+#### Boosting ####
+set.seed(1)
+# "gaussian" for regression, "bernoulli" for binary classification
+boost_boston <- gbm(medv ~ ., data = Boston[train, ], distribution = "gaussian", 
+                    n.trees = 5000, interaction.depth = 4)
+summary(boost_boston)
+
+# (?) partial dependence plot
+par(mfrow = c(1, 2))
+plot(boost_boston, i = "rm") # after "integrating out" other variables => positive relationship between medv and rm
+plot(boost_boston, i = "lstat")  # after "integrating out" other variables => negatie relationship between medv and lstat
+
+yhat_boost <- predict(boost_boston, newdata = Boston[-train, ], n.trees = 5000)
+sqrt(mean((yhat_boost - boston_test) ** 2)) # even lower test RMSE than rf and bagging
